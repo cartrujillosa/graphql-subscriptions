2011e85559876c17cc1d3a26a16ae548fba799eb
cartrujillo <carla.trujillo@bluetab.net>
2019-07-20 12:09:08 +0200

    feat: authentication

--- index.js
+++ index.js
@@ -1,23 +1,63 @@
 import mongoose from "mongoose";
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
+import userModel from "./models/user";
 
 import typeDefs from "./src/schema";
 import resolvers from "./src/resolvers";
 import http from "http";
+import jwt from "jsonwebtoken";
 
 const PORT = 3031;
 const MONGO_URL = "mongodb://localhost/node-graphql";
 const app = express();
 
+// TODO: crear servicio aparte que te devuelva el token y ya se comprueba aquí
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: ({ req, res }) => {
+    // get the user token from the headers
+    const token = req.headers.authorization || "";
+    const tokenDecoded = jwt.decode(token);
+    if (!tokenDecoded) {
+      res.json({ message: "Invalid token" });
+    }
+    const { id, exp } = tokenDecoded;
+    if (exp < Math.trunc(Date.now() / 1000)) {
+      res.json({ message: "Expired token" });
+    }
+
+    // try to retrieve a user with the token
+    findUser(id, function(error, userFound) {
+      if (error) res.status(422).json({ message: "Some error getting user" });
+      if (!userFound) res.json({ message: "User not found" });
+    });
+
+    // TODO: manage this warning with res.status
+    // (node:23072) UnhandledPromiseRejectionWarning: Unhandled promise rejection. This error originated either by throwing inside of an async function without a catch block, or by rejecting a promise which was not handled with .catch(). (rejection id: 1)
+    // (node:23072) [DEP0018] DeprecationWarning: Unhandled promise rejections are deprecated. In the future, promise rejections that are not handled will terminate the Node.js process with a non-zero exit code.
+    // add the user to the context
+    return { id };
+  }
 });
 
 server.applyMiddleware({
   app
 });
+
+const findUser = function(id, callback) {
+  // TODO: user this in other places and understand
+  userModel.findOne({ _id: id }, function(err, user) {
+    if (err) {
+      return callback(err);
+    } else if (user) {
+      return callback(null, user);
+    } else {
+      return callback();
+    }
+  });
+};
 
 const httpServer = http.createServer(app);
 server.installSubscriptionHandlers(httpServer);
--- models/user.js
+++ models/user.js
@@ -2,7 +2,13 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   email: String,
-  zircoins: Number
+  zircoins: Number,
+  social: {
+    googleProvider: {
+      id: String,
+      token: String
+    }
+  }
 });
 
 module.exports = mongoose.model("User", userSchema);
